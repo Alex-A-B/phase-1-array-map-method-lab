e57061376892eb2080904f86342c0552e0f3f9e2
Alex-A-B <alexjamesbelton@gmail.com>
2021-05-05 09:20:08 +0100

    Assignment completed - with additional notes

--- index.js
+++ index.js
@@ -11,6 +11,25 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// to pass the learn requirment - I had to remove the array modifier and put the return as purely tutorials.
+// this version is more robust and can capitalise other arrays not just the tutorials one presented here.
+const titleCased = (array) => { 
+  //returns the map of the whole array. Removing need to have a FOR loop.
+  //the arrow function then invokes a variable which = each line of array and splits to individual words
+  //placing in a new array.
+  return array.map( line => {
+    const splitInputArray = line.split(" ");
+  //a second variable is invoked which = the map of the new array of words,
+  //it then takes first letter of word and puts toUpperCase and holds the rest of the word back to be joined back
+    const capLetters = splitInputArray.map( word => 
+           word[0].toUpperCase() + word.substring(1) );
+  //a third variable then joins the capitalised word to remainder  by using secondVariable.join 
+    const capTitles = capLetters.join(" ");
+  //then returns updated array.  
+    return capTitles;        
+  });
 }
+
+
+console.table(titleCased())
+console.log(tutorials.length)
